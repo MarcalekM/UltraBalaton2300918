e366bd779358f943a3726da235b80998ff935472
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-18 09:30:08 +0200

    Add files via upload

---
+++ UltraBalaton230918/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UltraBalaton230918
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sportolok = new List<Sportolo> { };
+            var sr = new StreamReader(
+                path: @"..\..\..\src\ub2017egyeni.txt",
+                Encoding.UTF8);
+            while (!sr.EndOfStream) sportolok.Add(new Sportolo(sr.ReadLine()));
+
+            Console.WriteLine($"3. feladat: A versenyen {sportolok.Count()} egyéni versenyző indult");
+
+            var nt = sportolok
+                .Where(nt => !nt.Kategoria && nt.Tav == 100);
+            Console.WriteLine($"4. feladat: {nt.Count()} nő teljesítette a teljes távot");
+
+            Console.Write("Adja meg a sportolo nevét:  ");
+            string sn = Console.ReadLine();
+            foreach (var s in sportolok)
+            {
+                if 
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ UltraBalaton230918/Sportolo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraBalaton230918
+{
+    class Sportolo
+    {
+        public string Nev { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Kategoria { get; set; }
+        public DateTime Ido { get; set; }
+        public int Tav { get; set; }
+
+        public Sportolo(string r)
+        {
+            var v = r.Split(';');
+            Nev = v[0];
+            Rajtszam = int.Parse(v[1]);
+            Kategoria = v[2] == "Ferfi";
+            Ido = DateTime.ParseExact(v[3], "hh:mm:ss tt", System.Globalization.CultureInfo.CurrentCulture);
+            Tav = int.Parse(v[4]);
+        }
+    }
+}
