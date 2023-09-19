ab2377a08eb64cd7f907fa2905bdb38850fe48bd
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-09-19 08:19:29 +0200

    Update Sportolo.cs

--- UltraBalaton230918/Sportolo.cs
+++ UltraBalaton230918/Sportolo.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,19 +9,23 @@ namespace UltraBalaton230918
     class Sportolo
     {
         public string Nev { get; set; }
-        public int Rajtszam { get; set; }
+        public string Rajtszam { get; set; }
         public bool Kategoria { get; set; }
-        public DateTime Ido { get; set; }
-        public int Tav { get; set; }
+        public TimeSpan Versenyido { get; set; }
+        public int TavSzazalek { get; set; }
 
         public Sportolo(string r)
         {
             var v = r.Split(';');
             Nev = v[0];
-            Rajtszam = int.Parse(v[1]);
+            Rajtszam = v[1];
             Kategoria = v[2] == "Ferfi";
-            Ido = DateTime.ParseExact(v[3], "hh:mm:ss tt", System.Globalization.CultureInfo.CurrentCulture);
-            Tav = int.Parse(v[4]);
+            var ido = v[3].Split(';');
+            Versenyido = new TimeSpan(
+                hours: int.Parse(ido[0]),
+                minutes: int.Parse(ido[1]),
+                seconds: int.Parse(ido[2]));
+            TavSzazalek = int.Parse(v[4]);
         }
     }
 }
